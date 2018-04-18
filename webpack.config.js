fba113c97845d0b3476aef89dd1f64c639ca8cc4
marco <m.macelloni@asernet.it>
2018-04-18 13:06:00 +0200

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    entry: './dev/js',
+    mode: 'development',
+    entry: {
+        app: './dev/js/app.js',
+        vendors: './bower_components/jquery/dist/jquery.js'
+    },
     output: {
         path: path.resolve(__dirname, 'assets/js/'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     }
 };
